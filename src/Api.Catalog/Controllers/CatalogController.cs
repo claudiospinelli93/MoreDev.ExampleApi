2496bd2d96cd95657f7882a67239073261e084bf
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-10-12 12:36:45 -0300

    ✅ Add basic using api catalog controller.

---
+++ src/Api.Catalog/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Api Catalog";
+        }
+    }
+}
