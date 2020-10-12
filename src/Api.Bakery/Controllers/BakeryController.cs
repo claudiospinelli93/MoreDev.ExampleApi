a75d8a136d5e54ffb4f8a02d14495c79be3125ca
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-10-12 12:36:37 -0300

    ✅ Add basic using api bakery controller.

---
+++ src/Api.Bakery/Controllers/BakeryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Bakery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BakeryController : ControllerBase
+    {
+
+        private readonly ILogger<BakeryController> _logger;
+
+        public BakeryController(ILogger<BakeryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Api Bakery";
+        }
+    }
+}
